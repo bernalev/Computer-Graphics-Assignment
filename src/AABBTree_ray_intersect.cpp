02d62016067e5e19815f82732ca201cadf615bea
Eva Bernal <e.bernal@mail.utoronto.ca>
2019-10-07 13:03:31 -0400

    aabb tree intersect is up and working

--- src/AABBTree_ray_intersect.cpp
+++ src/AABBTree_ray_intersect.cpp
@@ -8,10 +8,44 @@ bool AABBTree::ray_intersect(
   double & t,
   std::shared_ptr<Object> & descendant) const 
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  t = 0;
-  return false;
-  ////////////////////////////////////////////////////////////////////////////
-}
+	// if ray doesn't even hit bounding box
+	if (!ray_intersect_box(ray, this->box, min_t, max_t))
+		return false;
+	
+	// page 303 of the textbook: pseudo code for hit traversal 
+	double t_l, t_r;
+	std::shared_ptr<Object> desc_l, desc_r;
+	
+	// Test whether cast failed (points to a primitive)
+	if (!std::dynamic_pointer_cast<AABBTree>(this->left))
+		desc_l = this->left; 	
+	if (!std::dynamic_pointer_cast<AABBTree>(this->right))
+		desc_r = this->right; 
 
+	bool left_hit = this->left->ray_intersect(ray, min_t, max_t, t_l, desc_l);
+	bool right_hit = this->right->ray_intersect(ray, min_t, max_t, t_r, desc_r);
+
+	if (left_hit && right_hit) {
+		if (t_l < t_r) {
+			t = t_l;
+			descendant = desc_l;
+		}
+		else {
+			t = t_r;
+			descendant = desc_r;
+		}
+		return true;
+	}
+	else if (left_hit) {
+		t = t_l;
+		descendant = desc_l;
+		return true;
+	}
+	else if (right_hit) {
+		t = t_r;
+		descendant = desc_r;
+		return true;
+	}
+	else
+		return false; 
+}
